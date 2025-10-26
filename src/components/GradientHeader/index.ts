b07e7f3dfc03ac5e7614b008f4438a818c10475c
Tim Maurizio <tim.maurizio@gmail.com>
2025-10-26 22:51:34 +0100

    fix(): camelcase

--- src/components/GradientHeader/index.ts
+++ src/components/GradientHeader/index.ts
@@ -1,6 +1,1 @@
-<<<<<<< HEAD
 export { gradientHeader } from "./gradientHeader";
-export type { gradientHeaderProps } from "./types";
-=======
-export { GradientHeader } from "./GradientHeader";
->>>>>>> a4b523ae8466e98e86105c25bdc2fa288c5554ad
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export * from "./components";
-=======
-export { GradientHeader } from "./components/GradientHeader";
->>>>>>> a4b523ae8466e98e86105c25bdc2fa288c5554ad
+export { gradientHeader } from "./components/gradientHeader";
